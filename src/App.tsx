b28b6e5f3e4e1fb85dc19a9f8eae2ec5ee785044
Robin Kooyman <robin.kooyman.work@gmail.com>
2025-05-04 20:12:46 +0200

    Init studio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "./app/connect/page"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh w-full">
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
