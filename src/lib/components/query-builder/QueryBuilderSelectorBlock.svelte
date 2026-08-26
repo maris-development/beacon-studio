ead5e2545fb1eb6f7bafb28b28d725357fa5749e
PC-JASPER\jasper <jasper@maris.nl>
2026-08-26 11:25:33 +0200

    fix: QueryBuilderSelectorBlock reference route card.svelte

--- src/lib/components/query-builder/QueryBuilderSelectorBlock.svelte
+++ src/lib/components/query-builder/QueryBuilderSelectorBlock.svelte
@@ -7,7 +7,7 @@ add new query blocks, duplicate blocks, close clocks, select active blocks
 <script lang="ts">
 	import Button from '$lib/components/buttons/Button.svelte';
 	import { Badge } from '$lib/components/ui/badge/index.js';
-	import Card from '@/components/card/Card.svelte';
+	import Card from '../card/card.svelte';
 	import CirclePlusIcon from '@lucide/svelte/icons/circle-plus';
 	import CircleCheckIcon from '@lucide/svelte/icons/circle-check';
 	import CopyIcon from '@lucide/svelte/icons/copy';
